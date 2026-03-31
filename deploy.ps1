959bc0024286dadf8eca8f2e2de448866c644379
notVLNT <211336718+notVLNT@users.noreply.github.com>
2026-03-31 11:53:45 +0200

    Delete deploy.ps1

--- deploy.ps1
+++
@@ -1,6 +0,0 @@
-param($msg = "update CSS")
-git add .
-git commit -m $msg
-git push
-Invoke-WebRequest -Uri "https://purge.jsdelivr.net/gh/notVLNT/goalher-assets@main/goalher-main.css" -UseBasicParsing | Out-Null
-Write-Host "✅ Deploy completato: $msg"
